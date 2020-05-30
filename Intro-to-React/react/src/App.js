51124e846d777c75cf9db032bd4713b4a97821b7
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-05-30 00:07:44 -0500

    First push.  Tells me map is not a function

--- Intro-to-React/react/src/App.js
+++ Intro-to-React/react/src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
 import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "reactstrap";
+import Character from "./components/Character"
 
 const App = () => {
+const [char, setChar] = useState([]);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  useEffect(() => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  axios.get("https://rickandmortyapi.com/api/character")
+  .then(response => {
+    console.log(response.data)
+    setChar(response.data);
+  }
 
+  )
+  .catch(err => {console.log("something went wrong")})
+
+.then(() => {console.log("success")})
+
+}, []);
+console.log(char)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      
+
+      <Character characterArray ={char}/>
     </div>
+
   );
 }
+
 
 export default App;
--- Intro-to-React/react/src/components/Character.js
+++ Intro-to-React/react/src/components/Character.js
@@ -1,1 +1,36 @@
-// Write your Character component here
+import React from "react";
+import { Container, Col, Row,Card, CardHeader, CardFooter, CardBody,
+ CardText } from 'reactstrap';
+ 
+ // Write your Character component here
+function Character (props) {
+  console.log(props.characterArray)
+
+    return(
+        <div>
+        {props.characterArray.map((character) => {
+         return(    
+
+<Col xs="3">
+<Card key = {character.created} >
+      <CardHeader>{character.name}</CardHeader>
+      <CardBody>
+      <img
+          alt="post thumbnail"
+         className="post-image"
+         src={props.Picture}
+       />
+        
+        <CardText>{props.status}</CardText>
+        <CardText>{props.species}</CardText>
+      {/* //  <Button>Go somewhere</Button> */}
+      </CardBody>
+      <CardFooter>{`Created: ${props.Date}`}
+      </CardFooter>
+    </Card>
+</Col>)})}
+</div>
+         );}
+ 
+
+export default Character;
